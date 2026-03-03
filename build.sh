0f207946bfae2cd4b0753cba8cee7f7322dc977e
RaulCsar <rccs6@discente.ifpe.edu.br>
2026-03-03 07:54:59 -0300

    fix: vercel logic 2

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Cria e ativa um ambiente virtual no servidor do Vercel
+python3 -m venv venv
+source venv/bin/activate
+
+# Instala as dependências dentro do ambiente isolado e executa o script
 pip install -r requirements.txt
-python scraper.py
+python3 scraper.py
